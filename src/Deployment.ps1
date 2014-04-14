5ffae45650cfaabcc54b5a00321e72b7e3475c77
Sleeper Smith <jason.chen@bit-clouded.com>
2014-04-15 02:34:47 +1000

    Added a template for two subnets across two AZ. Added a vpn template that provision the trio of gateway/gateway/vpn connection.

--- src/Deployment.ps1
+++ src/Deployment.ps1
@@ -1,25 +1,56 @@
-﻿function New-StackLink {
-    param(
-        [string]$TemplateUrl,
-        $tags = @()
-    )
+﻿# Name > Environment > LogicalUnit
 
+function New-StackLink(
+        [Parameter(
+            Mandatory = $true)]
+        [string]$TemplateUrl,
+
+        [string]$StackName,
+        $Tags = @(),
+        $StackParameters = @(),
+
+        [string]$TemplateUrlBaseParameterKey = "TemplateBaseUrl"
+    ) {
+    Write-Host
+    Write-Host "## Start New-StackLink ##"
+
+    # Metadata
+    Write-Host == Metadata ==
     $templateUrlSegments = $TemplateUrl.Split('/')
     $templateName = $templateUrlSegments[$templateUrlSegments.Length - 1].Split('.')[0]
     Write-Host CloudFormation template name: $templateName
+    if ([string]::IsNullOrWhiteSpace($StackName)) {
+        $StackName = $templateName
+    }
+    Write-Host CloudFormation stack name: $StackName
+    $templateUrlSegments[$templateUrlSegments.Count-1] = ""
+    $templateUrlBase = [string]::Join("/", $templateUrlSegments)
+    Write-Host CloudFormation template base path: $templateUrlBase
 
     $cfnNewLaunchTarget = Test-CFNTemplate -TemplateURL $TemplateUrl
     $cfnNewLaunchTargetParameterKeys = $cfnNewLaunchTarget.Parameters | % {
         $_.ParameterKey
     }
 
+    # Get existing stacks
+    Write-Host == Existing Stacks ==
     $templateNameTag = "TemplateName"
     $cfnStacks = Get-CFNStack | ? {
         ($_.Tags | ? { $_.Key -eq $templateNameTag }).Count -eq 1
     }
-    $cfnStacks.Count
+    if ($cfnStacks.Count -eq 0) {
+        Write-Host No stacks found.
+    } else {
+        Write-Host $([string]::Join(", ", ($cfnStacks | % { $_.StackName })))
+    }
 
-    $dependantParameters = @()
+    # Add dependant parameters
+    Write-Host == Parameters ==
+    if ($cfnNewLaunchTargetParameterKeys.Contains($TemplateUrlBaseParameterKey)) {
+        $StackParameters += @{
+            "Key" = $TemplateUrlBaseParameterKey; "Value" = $templateUrlBase
+        }
+    }
     foreach($cfnStack in $cfnStacks) {
         $cfnStackTemplateName = $cfnStack.Tags | ? {
             $_.Key -eq $templateNameTag
@@ -30,7 +61,7 @@
         $cfnStack[0].Parameters | % {
             $key = $cfnStackTemplateName + "In" + $_.Key
             if ($cfnNewLaunchTargetParameterKeys.Contains($key)) {
-                $dependantParameters += @{
+                $StackParameters += @{
                     "Key" = $key; "Value" = $_.Value
                 }
             }
@@ -38,30 +69,34 @@
         $cfnStack[0].Outputs | % {
             $key = $cfnStackTemplateName + "Out" + $_.OutputKey
             if ($cfnNewLaunchTargetParameterKeys.Contains($key)) {
-                $dependantParameters += @{
+                $StackParameters += @{
                     "Key" = $key; "Value" = $_.OutputValue
                 }
             }
         }
     }
 
-    $dependantParameters | % {
-        $_
+    $StackParameters | % {
+        Write-Host ">" $_["Key"]: $_["Value"]
     }
-    
-    $tags += @{"Key" = $templateNameTag; "Value" = $templateName}
-    New-CFNStack -StackName $templateName -Parameters $dependantParameters -TemplateURL $TemplateUrl -Tags $tags
+
+    Write-Host == Create Stack ==    
+    $Tags += @{"Key" = $templateNameTag; "Value" = $templateName}
+    $stackId = New-CFNStack -StackName $StackName -Parameters $StackParameters -TemplateURL $TemplateUrl -Tags $tags
+    Write-Host StackArn: $stackId
+    Write-Host "## End New-StackLink ##"
+    return $stackId
 }
 
-function Wait-StackLink {
-    param(
+function Wait-StackLink(
         [Parameter(
             Mandatory = $true,
-            ValueFromPipeline = $true,
-            ValueFromPipelineByPropertyName=$true)]
+            ValueFromPipeline = $true)]
         [string]$StackLinkId,
         [int]$PollInterval = 15
-    )
+    ) {
+    Write-Host
+    Write-Host "## Start Wait-StackLink ##"
 
     $statusToBlock = @(
         [Amazon.CloudFormation.StackStatus]::CREATE_IN_PROGRESS,
@@ -74,4 +109,5 @@ function Wait-StackLink {
         Write-Host "Waiting... Stack status: " (Get-CFNStack $StackLinkId).StackStatus
         sleep $PollInterval
     }
+    Write-Host "## End Wait-StackLink ##"
 }
