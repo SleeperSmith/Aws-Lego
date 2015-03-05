dc874ec645bbf8b39dbcad623240b811f19a0dc7
Sleeper Smith <jason.chen@bit-clouded.com>
2015-03-05 22:49:16 +1100

    3 Scripts for 3 kinds of reference network topology.

---
+++ src/Create-CompleteReferenceTopology.ps1
@@ -0,0 +1,34 @@
+﻿param(
+    $prefix = "https://s3-ap-southeast-2.amazonaws.com/bc-deployment/AWS-Lego/Alpha/"
+)
+
+.".\Deployment.ps1"
+
+$tags = @(
+    @{"Key" = "Project"; "Value" = "Infrastructure"},
+    @{"Key" = "Environment"; "Value" = "Prod"}
+)
+
+Get-StackLinkParameters -TemplateUrl "$($prefix)basic-blocks/vpc.template" |
+    Upsert-StackLink -Tags $tags -StackName Test-PrimaryVpc |
+    Wait-StackLink
+
+Get-StackLinkParameters -TemplateUrl "$($prefix)basic-blocks/internet-access.subnets.template" |
+    Upsert-StackLink -Tags $tags -StackName Test-GatewaySubnets |
+    Wait-StackLink
+
+Get-StackLinkParameters -TemplateUrl "$($prefix)basic-blocks/webserver.subnets.template" |
+    Upsert-StackLink -Tags $tags -StackName Test-WebServerSubnets |
+    Wait-StackLink
+
+Get-StackLinkParameters -TemplateUrl "$($prefix)basic-blocks/private.subnets.template" |
+    Upsert-StackLink -Tags $tags -StackName Test-PrivateSubnets |
+    Wait-StackLink
+
+Get-StackLinkParameters -TemplateUrl "$($prefix)basic-blocks/nat-enabled.subnets.template" |
+    Upsert-StackLink -Tags $tags -StackName Prod-NatSubnets |
+    Wait-StackLink
+
+Get-StackLinkParameters -TemplateUrl "$($prefix)basic-blocks/elb.subnets.template" |
+    Upsert-StackLink -Tags $tags -StackName Prod-ElbSubnets |
+    Wait-StackLink
---
+++ src/Create-PublicReferenceTopology.ps1
@@ -0,0 +1,25 @@
+﻿param(
+    $prefix = "https://s3-ap-southeast-2.amazonaws.com/bc-deployment/AWS-Lego/Alpha/"
+)
+
+.".\Deployment.ps1"
+$tags = @(
+    @{"Key" = "Project"; "Value" = "Infrastructure"},
+    @{"Key" = "Environment"; "Value" = "Prod"}
+)
+
+Get-StackLinkParameters -TemplateUrl "$($prefix)basic-blocks/vpc.template" |
+    Upsert-StackLink -Tags $tags -StackName Test-PrimaryVpc |
+    Wait-StackLink
+
+Get-StackLinkParameters -TemplateUrl "$($prefix)basic-blocks/internet-access.subnets.template" |
+    Upsert-StackLink -Tags $tags -StackName Test-GatewaySubnets |
+    Wait-StackLink
+
+Get-StackLinkParameters -TemplateUrl "$($prefix)basic-blocks/private.subnets.template" |
+    Upsert-StackLink -Tags $tags -StackName Test-PrivateSubnets |
+    Wait-StackLink
+
+Get-StackLinkParameters -TemplateUrl "$($prefix)basic-blocks/nat-enabled.subnets.template" |
+    Upsert-StackLink -Tags $tags -StackName Prod-NatSubnets |
+    Wait-StackLink
