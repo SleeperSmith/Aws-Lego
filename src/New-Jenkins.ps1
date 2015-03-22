3163be43053a09e0445744ca25fbd35a634a6e2a
Sleeper Smith <jason.chen@bit-clouded.com>
2015-03-22 17:40:33 +1100

    Scripts for launching jenkins.

---
+++ src/New-Jenkins.ps1
@@ -0,0 +1,19 @@
+﻿param(
+    $prefix = "https://s3-ap-southeast-2.amazonaws.com/bc-public-releases/AWS-Lego/Alpha/",
+    $kp = (Read-Host 'Name of Key Pair to user for Go Server instances access'),
+    $ami = (Read-Host 'AMI ID of Go Master Server (Ubuntu)'),
+    $winami = (Read-Host 'AMI ID of Go Windows Build Agent (Windows)'),
+    $tags = @(
+        @{"Key" = "Project"; "Value" = "Infrastructure"},
+        @{"Key" = "Environment"; "Value" = "Prod"}
+    )
+)
+
+.".\Deployment.ps1"
+
+Get-StackLinkParameters -TemplateUrl "$($prefix)special-blocks/jenkins.template" -StackParameters @(
+    @{"Key" = "KeyPairName"; "Value" = $kp},
+    @{"Key" = "ServerAmi"; "Value" = $ami},
+    @{"Key" = "LinuxAgentAmi"; "Value" = $ami},
+    @{"Key" = "WindowsAgentAmi"; "Value" = $winami}
+) | Upsert-StackLink -Tags $tags -StackName "Prod-Jenkins"
