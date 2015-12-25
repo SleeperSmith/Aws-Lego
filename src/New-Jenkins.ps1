602f112a244494c7ae9394eb6bde843c0c584ad1
Jason Chen <jason.chen@bit-clouded.com>
2015-12-26 00:02:10 +1100

    Typo fix.

--- src/New-Jenkins.ps1
+++ src/New-Jenkins.ps1
@@ -1,8 +1,8 @@
 ﻿param(
     $prefix = "https://s3-ap-southeast-2.amazonaws.com/bc-public-releases/AWS-Lego/Alpha/",
-    $kp = (Read-Host 'Name of Key Pair to user for Go Server instances access'),
-    $ami = (Read-Host 'AMI ID of Go Master Server (Ubuntu)'),
-    $winami = (Read-Host 'AMI ID of Go Windows Build Agent (Windows)'),
+    $kp = (Read-Host 'Name of Key Pair to user for Jenkins Server instances access'),
+    $ami = (Read-Host 'AMI ID of Jenkins Master Server (Ubuntu)'),
+    $winami = (Read-Host 'AMI ID of Jenkins Windows Build Agent (Windows)'),
     $tags = @(
         @{"Key" = "Project"; "Value" = "Infrastructure"},
         @{"Key" = "Environment"; "Value" = "Prod"}
