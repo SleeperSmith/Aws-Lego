b20c2587ffeebdefa291e249795cd082633078ef
Jason Chen <jason.chen@bit-clouded.com>
2016-03-13 21:48:05 +1100

    Updated script to reflect the update on NAT gateway.

--- src/New-CompleteReferenceTopology.ps1
+++ src/New-CompleteReferenceTopology.ps1
@@ -1,7 +1,5 @@
 ﻿param(
     $prefix = "https://s3-ap-southeast-2.amazonaws.com/bc-public-releases/AWS-Lego/Alpha/",
-    $natkp = (Read-Host 'Name of Key Pair to user for NAT server instance access'),
-    $natAmi = (Read-Host 'AMI ID of NAT'),
     $tags = @(
         @{"Key" = "Project"; "Value" = "Infrastructure"},
         @{"Key" = "Environment"; "Value" = "Prod"}
@@ -26,10 +24,8 @@ Get-StackLinkParameters -TemplateUrl "$($prefix)basic-blocks/private.subnets.tem
     Upsert-StackLink -Tags $tags -StackName Prod-PrivateSubnets |
     Wait-StackLink
 
-Get-StackLinkParameters -TemplateUrl "$($prefix)basic-blocks/nat-enabled.subnets.template" -StackParameters @(
-    @{"Key" = "KeyPairName"; "Value" = $natkp},
-    @{"Key" = "NatImageId"; "Value" = $natAmi}
-) | Upsert-StackLink -Tags $tags -StackName Prod-NatSubnets |
+Get-StackLinkParameters -TemplateUrl "$($prefix)basic-blocks/nat-enabled.subnets.template" |
+    Upsert-StackLink -Tags $tags -StackName Prod-NatSubnets |
     Wait-StackLink
 
 Get-StackLinkParameters -TemplateUrl "$($prefix)basic-blocks/elb.subnets.template" |
